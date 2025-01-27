9f8587613a076e71557f0d2e169ca8e0cb2d35dc
Nicolas <duchenne.n@gmail.com>
2025-01-27 10:37:21 +0100

    code fonctionnel avec documentation

--- pipelines/config/config.py
+++ pipelines/config/config.py
@@ -1,3 +1,8 @@
 from dotenv import load_dotenv
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# Construct the path to the .env file
+dotenv_path = os.path.join(current_dir, '.env')
 
-load_dotenv()
+# Load the .env file
+load_dotenv(dotenv_path)
