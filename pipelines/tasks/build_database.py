c54769c4153000f1ae251fad50c1376b437d45f3
pppp56 <50910271+moreaupascal56@users.noreply.github.com>
2025-02-05 11:20:16 +0100

    Feature/collecte de plusieurs annees pour le dataset edc (#9)
    * add fct get_yearly_dataset_infos
    
    * parametrize process_sise_eaux_dataset_2024
    
    * recreate cache folder by default in clear_cache()
    
    * add check_table_existence function
    
    * parametrize process_sise_eaux_dataset_2024()
    
    * format _common.py
    
    * add process_sise_eaux_dataset() controller and rename process_sise_eaux_dataset_2024 to download_extract_insert_yearly_SISE_data
    
    * upd docstrings, formatting
    
    * upd logs and add a clear_cache() in process_sise_eaux_dataset
    
    * reorganize file
    
    * add notebook to preview data
    
    * fix Incompatible types in assignment
    
    * rename SISE to EDC
    
    * rename annee_prelevement to de_partition
    
    * catch and raise error if refresh_type not in the allowed values
    
    * format
    
    * fix typo
    
    * add _config_edc.py and use it accordingly
    
    * add de_ingestion_date
    
    * make refresh_type="last" as default
    
    * fix example notebooks with new sql table names
    
    * delete test_plusierus_annees.ipynb
    
    ---------
    
    Co-authored-by: Jeremy Greze <jereze@users.noreply.github.com>

--- pipelines/tasks/build_database.py
+++ pipelines/tasks/build_database.py
@@ -4,58 +4,148 @@ Consolidate data into the database.
 
 import logging
 import os
+from typing import List, Literal
 from zipfile import ZipFile
 
 import duckdb
 import requests
 
 from ._common import CACHE_FOLDER, DUCKDB_FILE, clear_cache
+from ._config_edc import get_edc_config, create_edc_yearly_filename
 
 logger = logging.getLogger(__name__)
+edc_config = get_edc_config()
 
 
-def process_sise_eaux_dataset_2024():
-    """Process SISE-Eaux dataset for 2024."""
+def check_table_existence(conn: duckdb.DuckDBPyConnection, table_name: str) -> bool:
+    """
+    Check if a table exists in the duckdb database
+    :param conn: The duckdb connection to use
+    :param table_name: The table name to check existence
+    :return: True if the table exists, False if not
+    """
+    query = f"""
+        SELECT COUNT(*)
+        FROM information_schema.tables
+        WHERE table_name = '{table_name}'
+        """
+    conn.execute(query)
+    return list(conn.fetchone())[0] == 1
+
+
+def download_extract_insert_yearly_edc_data(year: str):
+    """
+    Downloads from www.data.gouv.fr the EDC (Eau distribuée par commune) dataset for one year,
+    extracts the files and insert the data into duckdb
+    :param year: The year from which we want to download the dataset
+    :return: Create or replace the associated tables in the duckcb database.
+        It adds the column "de_partition" based on year as an integer.
+    """
 
     # Dataset specific constants
     DATA_URL = (
-        "https://www.data.gouv.fr/fr/datasets/r/84a67a3b-08a7-4001-98e6-231c74a98139"
+        edc_config["source"]["base_url"]
+        + edc_config["source"]["yearly_files_infos"][year]["id"]
     )
-    ZIP_FILE = os.path.join(CACHE_FOLDER, "dis-2024.zip")
-    EXTRACT_FOLDER = os.path.join(CACHE_FOLDER, "raw_data_2024")
+    ZIP_FILE = os.path.join(
+        CACHE_FOLDER, edc_config["source"]["yearly_files_infos"][year]["zipfile"]
+    )
+    EXTRACT_FOLDER = os.path.join(CACHE_FOLDER, f"raw_data_{year}")
+    FILES = edc_config["files"]
 
-    FILES = {
-        "communes": {"filename": "DIS_COM_UDI_2024.txt", "table": "sise_communes"},
-        "prelevements": {"filename": "DIS_PLV_2024.txt", "table": "sise_prelevements"},
-        "resultats": {"filename": "DIS_RESULT_2024.txt", "table": "sise_resultats"},
-    }
-
-    logger.info("Downloading and extracting SISE-Eaux dataset for 2024...")
+    logger.info(f"Processing EDC dataset for {year}...")
     response = requests.get(DATA_URL, stream=True)
     with open(ZIP_FILE, "wb") as f:
         for chunk in response.iter_content(chunk_size=8192):
             f.write(chunk)
 
-    logger.info("Extracting files...")
+    logger.info("   Extracting files...")
     with ZipFile(ZIP_FILE, "r") as zip_ref:
         zip_ref.extractall(EXTRACT_FOLDER)
 
-    logger.info("Creating tables in the database...")
+    logger.info("   Creating or updating tables in the database...")
     conn = duckdb.connect(DUCKDB_FILE)
+
     for file_info in FILES.values():
-        filepath = os.path.join(EXTRACT_FOLDER, file_info["filename"])
-        query = f"""
-            CREATE OR REPLACE TABLE {file_info["table"]} AS 
-            SELECT * FROM read_csv('{filepath}', header=true, delim=',');
+        filepath = os.path.join(
+            EXTRACT_FOLDER,
+            create_edc_yearly_filename(
+                file_name_prefix=file_info["file_name_prefix"],
+                file_extension=file_info["file_extension"],
+                year=year,
+            ),
+        )
+
+        if check_table_existence(conn=conn, table_name=f"{file_info['table_name']}"):
+            query = f"""
+                DELETE FROM {f"{file_info['table_name']}"}
+                WHERE de_partition = CAST({year} as INTEGER)
+                ;
+            """
+            conn.execute(query)
+            query_start = f"INSERT INTO {f'{file_info["table_name"]}'} "
+
+        else:
+            query_start = f"CREATE TABLE {f'{file_info["table_name"]}'} AS "
+
+        query_select = f"""
+            SELECT 
+                *,
+                CAST({year} AS INTEGER) AS de_partition,
+                current_date            AS de_ingestion_date
+            FROM read_csv('{filepath}', header=true, delim=',');
         """
-        conn.execute(query)
+
+        conn.execute(query_start + query_select)
+
     conn.close()
 
-    logger.info("Cleaning up...")
+    logger.info("   Cleaning up cache...")
     clear_cache()
 
     return True
 
 
+def process_edc_datasets(
+    refresh_type: Literal["all", "last", "custom"] = "last",
+    custom_years: List[str] = None,
+):
+    """
+    Process the EDC datasets.
+    :param refresh_type: Refresh type to run
+        - "all": Refresh the data for every possible year
+        - "last": Refresh the data only for the last available year
+        - "custom": Refresh the data for the years specified in the list custom_years
+    :param custom_years: years to update
+    :return:
+    """
+    available_years = edc_config["source"]["available_years"]
+
+    if refresh_type == "all":
+        years_to_update = available_years
+    elif refresh_type == "last":
+        years_to_update = available_years[-1:]
+    elif refresh_type == "custom":
+        if custom_years:
+            years_to_update = list(set(custom_years).intersection(available_years))
+        else:
+            raise ValueError(
+                """ custom_years parameter needs to be specified if refresh_type="custom" """
+            )
+    else:
+        raise ValueError(
+            f""" refresh_type needs to be one of ["all", "last", "custom"], it can't be: {refresh_type}"""
+        )
+
+    logger.info(f"Launching processing of EDC datasets for years: {years_to_update}")
+
+    for year in years_to_update:
+        download_extract_insert_yearly_edc_data(year=year)
+
+    logger.info("Cleaning up cache...")
+    clear_cache(recreate_folder=False)
+    return True
+
+
 def execute():
-    process_sise_eaux_dataset_2024()
+    process_edc_datasets()
